9b13f18c71ae9f992613b80dc9ccee26c0fa9f67
Nick ten Veen <nveen0930@gmail.com>
2016-07-07 17:48:09 +0200

    added simple react todo table with data from backend

--- assets/js/frameworks/react-mobx/applications/todo/index.js
+++ assets/js/frameworks/react-mobx/applications/todo/index.js
@@ -1,4 +1,51 @@
-import React from 'react';
+import { Component } from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import $ from 'jquery';
 
-export default () => <div>TODO</div>;
+const Loading = () => {
+  return <div>loading...</div>
+}
+
+const Todos = ({todos}) => {
+  return <table className="pure-table">
+    <thead>
+      <th>
+        Task
+      </th>
+      <th>
+        Status
+      </th>
+    </thead>
+    {
+      todos.map(todo =>
+        <tr key={todo._id}>
+          <td>
+            {todo.task}
+          </td>
+          <td>
+            {todo.finished ? 'finished' : 'pending'}
+          </td>
+        </tr>
+      )
+    }
+  </table>;
+}
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  componentDidMount(){
+    $.getJSON('/api/todos', (todos) =>
+      this.setState({todos})
+    );
+  }
+
+  render(){
+    const {
+      todos
+    } = this.state;
+
+    return todos ? <Todos {...{todos}}/> : <Loading/>
+  }
+}
